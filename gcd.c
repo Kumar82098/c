d25c477195ce019afcca2acfc278607a9c547edc
Kumar82098 <56931055+Kumar82098@users.noreply.github.com>
2019-10-23 22:03:13 +0530

    upload file

---
+++ gcd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,i,j;
+ printf("Enter 2 numbers: ");
+ scanf("%d%d",&a,&b);
+ for(i=1;i<=a;i++)
+ {
+  if(a%i==0)
+  return i;
+  else 
+    return 0;
+    
+ }
+ for(j=1;j<=b;j++)
+ {
+  if(b%j==0)
+  return j;
+  else 
+    return 0;
+ }
+ 
+return 0;    
+}
